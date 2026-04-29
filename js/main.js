5be8dc5d652246f2e476214e9a296215b64e1a35
David Santamaría Rogado <howl.nsp@gmail.com>
2026-04-29 10:18:03 +0200

    do exercise 3
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -21,3 +21,15 @@ console.log(separaEnMayusculas('Ana'));
 const cuentaPalabras = nombre => nombre.split(' ').length;
 
 console.log(cuentaPalabras('Antonio Alberto Jesús'));
+
+/**
+ * Ejercicio 3:
+ * Diseña un algoritmo que cuente las veces que aparece una determinada letra
+ * en una frase.
+ * @param {string} frase
+ * @param {string} letra
+ * @returns
+ */
+const cuentaLetras = (frase, letra) => frase.match(new RegExp(letra, 'gi')).length;
+
+console.log(cuentaLetras('No es que tenga poco dinero, es que tengo un exceso de mes al final del sueldo.', 'o'));
